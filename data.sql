959e84e136051c39e09fa01927d969000e31fe17
oujjet <81743948+salim-OU@users.noreply.github.com>
2026-03-24 00:08:25 +0100

    Update roles insertion with new IDs and names

--- data.sql
+++ data.sql
@@ -1,3 +1,3 @@
--- Roles
-INSERT INTO roles (name) VALUES 
-('ROLE_ADMIN'), ('ROLE_USER'), ('ROLE_AUDITOR');
+INSERT INTO roles (id, name) VALUES (1, 'ADMIN');
+INSERT INTO roles (id, name) VALUES (2, 'AUDITOR');
+INSERT INTO roles (id, name) VALUES (3, 'CUSTOMER');
